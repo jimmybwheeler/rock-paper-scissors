a0270171e61474d0d38b9f7c8ef5ac46f5f601c8
jimmy <jimmybwheeler@gmail.com>
2022-06-29 21:57:55 -0700

    start play function, not working

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ function computerPlay(){
 }
 
 function play(player, comp){
-
+    if (player === "rock" && comp === "0"){
+        console.log("Tie");
+    } else if (player === "rock" && comp === "1"){
+        console.log("Paper beats rock, computer wins!");
+    } else if (player === "rock" && comp === "2"){
+        console.log("Rock beats scissors, player wins!");
+    }
 }
 
 let playerSelection = prompt("Rock, paper or scissors?")
