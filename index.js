4c9b3afc9e3bea2f69a45ae10474c70c74a36989
jimmy <jimmybwheeler@gmail.com>
2022-06-29 22:07:55 -0700

    add paper options in play function

--- index.js
+++ index.js
@@ -5,16 +5,22 @@ function computerPlay(){
 }
 
 function play(player, comp){
-    if (player == "rock" && comp == "0"){
+    if (player.toLowerCase() == "rock" && comp == "0"){
         console.log("Tie");
-    } else if (player == "rock" && comp == "1"){
+    } else if (player.toLowerCase() == "rock" && comp == "1"){
         console.log("Paper beats rock, computer wins!");
-    } else if (player == "rock" && comp == "2"){
+    } else if (player.toLowerCase() == "rock" && comp == "2"){
         console.log("Rock beats scissors, player wins!");
+    } else if (player.toLowerCase() == "paper" && comp == "0"){
+        console.log("Paper beats rock, player wins!");
+    } else if (player.toLowerCase() == "paper" && comp == "1"){
+        console.log("Tie!");
+    } else if (player.toLowerCase() == "paper" && comp == "2"){
+        console.log("Scissors beats paper, computer wins!");
     }
 }
 
-let playerSelection = prompt("Rock, paper or scissors?")
+let playerSelection = prompt("Rock, paper or scissors?");
 
 let computerSelection = computerPlay();
 
