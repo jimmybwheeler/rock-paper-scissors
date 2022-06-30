6bd81204671d85bc874c7c64230568dc81d952a0
jimmy <jimmybwheeler@gmail.com>
2022-06-29 22:10:12 -0700

    add scissors options in play function

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function computerPlay(){
 
 function play(player, comp){
     if (player.toLowerCase() == "rock" && comp == "0"){
-        console.log("Tie");
+        console.log("Tie!");
     } else if (player.toLowerCase() == "rock" && comp == "1"){
         console.log("Paper beats rock, computer wins!");
     } else if (player.toLowerCase() == "rock" && comp == "2"){
@@ -17,6 +17,12 @@ function play(player, comp){
         console.log("Tie!");
     } else if (player.toLowerCase() == "paper" && comp == "2"){
         console.log("Scissors beats paper, computer wins!");
+    } else if (player.toLowerCase() == "scissors" && comp == "0"){
+        console.log("Rock beats scissors, computer wins!");
+    } else if (player.toLowerCase() == "scissors" && comp == "1"){
+        console.log("Scissors beats paper, player wins!");
+    } else if (player.toLowerCase() == "scissors" && comp == "2"){
+        console.log("Tie!");
     }
 }
 
