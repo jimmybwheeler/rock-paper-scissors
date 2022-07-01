3420953afb8d22da628645ae9ea9cd57cc6af4a7
jimmy <jimmybwheeler@gmail.com>
2022-06-30 23:44:38 -0700

    added variables to game, not working

--- index.js
+++ index.js
@@ -8,18 +8,30 @@ function play(player, comp){
     if (player.toLowerCase() == "rock" && comp == "0"){
         console.log("Tie!");
     } else if (player.toLowerCase() == "rock" && comp == "1"){
+        let compWin = 1;
+        return compWin;
         console.log("Paper beats rock, computer wins!");
     } else if (player.toLowerCase() == "rock" && comp == "2"){
+        let playerWin = 1;
+        return playerWin;
         console.log("Rock beats scissors, player wins!");
     } else if (player.toLowerCase() == "paper" && comp == "0"){
+        let playerWin = 1;
+        return playerWin;
         console.log("Paper beats rock, player wins!");
     } else if (player.toLowerCase() == "paper" && comp == "1"){
         console.log("Tie!");
     } else if (player.toLowerCase() == "paper" && comp == "2"){
+        let compWin = 1;
+        return compWin;
         console.log("Scissors beats paper, computer wins!");
     } else if (player.toLowerCase() == "scissors" && comp == "0"){
+        let compWin = 1;
+        return compWin;
         console.log("Rock beats scissors, computer wins!");
     } else if (player.toLowerCase() == "scissors" && comp == "1"){
+        let playerWin = 1;
+        return playerWin;
         console.log("Scissors beats paper, player wins!");
     } else if (player.toLowerCase() == "scissors" && comp == "2"){
         console.log("Tie!");
@@ -30,7 +42,19 @@ function play(player, comp){
 
 function game(player, comp){
     for (let i = 0; i < 5; i++){
-        play(player, comp);
+        let score = play(player, comp);
+        let playerWin;
+        let compWin;
+        if (score == playerWin){
+            let playerScore = playerScore + 1;
+            return playerScore;
+        } else if (score == compWin){
+            let compScore = compScore + 1;
+            return compScore;
+        } else {
+            alert("Tie")
+            break;
+        }
     }
 }
 
