fb7d65ac70fc699e05015b33be081ac6e9bffb4f
James Wheeler <jimmybwheeler@gmail.com>
2022-07-02 14:01:34 -0700

    finished play round function

--- index.js
+++ index.js
@@ -5,10 +5,52 @@ function computerPlay(){
 }
 
 function playRound(player, computer){
-    
+    let result;
+    if(player.toLowerCase() == "rock" && computer == "0"){
+        console.log("Tie.");
+        result = "no winner";
+        return result;
+    } else if(player.toLowerCase() == "rock" && computer == "1"){
+        console.log("Paper beats rock, computer wins!");
+        result = "computer wins";
+        return result;
+    } else if(player.toLowerCase() == "rock" && computer == "2"){
+        console.log("Rock beats scissors, player wins!");
+        result = "player wins";
+        return result;
+    } else if(player.toLowerCase() == "paper" && computer == "0"){
+        console.log("Paper beats rock, player wins!");
+        result = "player wins";
+        return result;
+    } else if(player.toLowerCase() == "paper" && computer == "1"){
+        console.log("Tie.");
+        result = "no winner";
+        return result;
+    } else if(player.toLowerCase() == "paper" && computer == "2"){
+        console.log("Scissors beats paper, computer wins!");
+        result = "computer wins";
+        return result;
+    } else if(player.toLowerCase() == "scissors" && computer == "0"){
+        console.log("Rock beats scissors, computer wins!");
+        result = "computer wins";
+        return result;
+    } else if(player.toLowerCase() == "scissors" && computer == "1"){
+        console.log("Scissors beats paper, player wins!");
+        result = "player wins";
+        return result;
+    } else if(player.toLowerCase() == "scissors" && computer == "2"){
+        console.log("Tie.");
+        result = "no winner";
+        return result;
+    } else {
+        console.log("Try something else!");
+        result = "wrong input";
+        return result;
+    }
 }
 
 const playerChoice = prompt("Rock, paper or scissors?");
 const computerChoice = computerPlay();
-
 const winner = playRound(playerChoice, computerChoice);
+
+console.log(winner);
