d61861028ca7b1566473771f00933be46ce4c5f8
James Wheeler <jimmybwheeler@gmail.com>
2022-07-25 13:40:24 -0700

    figured some things out, some not

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const divScore = document.querySelector(".score");
 const rockBtn = document.querySelector("#rock");
 const paperBtn = document.querySelector("#paper");
 const scissorsBtn = document.querySelector("#scissors");
+let playerWinCount = 0;
+let computerWinCount = 0;
 
 function computerPlay(){
     let choice = ["rock", "paper", "scissors"];
@@ -15,61 +17,67 @@ function playRound(player, computer){
     if(player == "rock" && computer == "0"){
         div.textContent = "Tie.";
         result = "no winner";
-        return result;
+        game(result);
+        
     } else if(player == "rock" && computer == "1"){
         div.textContent = "Paper beats rock, computer wins!";
         result = "computer wins";
-        return result;
+        game(result);
+        
     } else if(player == "rock" && computer == "2"){
         div.textContent = "Rock beats scissors, player wins!";
         result = "player wins";
-        return result;
+        game(result);
+        
     } else if(player == "paper" && computer == "0"){
         div.textContent = "Paper beats rock, player wins!";
         result = "player wins";
-        return result;
+        game(result);
+        
     } else if(player == "paper" && computer == "1"){
         div.textContent ="Tie.";
         result = "no winner";
-        return result;
+        game(result);
+        
     } else if(player == "paper" && computer == "2"){
         div.textContent ="Scissors beats paper, computer wins!";
         result = "computer wins";
-        return result;
+        game(result);
+        
     } else if(player == "scissors" && computer == "0"){
         div.textContent ="Rock beats scissors, computer wins!";
         result = "computer wins";
-        return result;
+        game(result);
+        
     } else if(player == "scissors" && computer == "1"){
         div.textContent ="Scissors beats paper, player wins!";
         result = "player wins";
-        return result;
+        game(result);
+        
     } else if(player == "scissors" && computer == "2"){
         div.textContent = "Tie.";
         result = "no winner";
-        return result;
+        game(result);
+        
     } else {
         div.textContent = "Try something else!";
         result = "wrong input";
-        return result;
+        game(result);
+        
     }
 }
 
 
-function game(choice, result){
-    let playerWinCount = 0;
-    let computerWinCount = 0;
-    
-    
+function game(result){
     const winner = result;
 
     if(winner == "player wins"){
         playerWinCount = playerWinCount + 1;
+        return playerWinCount;
     } else if(winner == "computer wins"){
         computerWinCount = computerWinCount + 1;
+        return computerWinCount;
     }
-
-    divScore.textContent = "player: " + playerWinCount + " " + "computer: " + computerWinCount;
     
     /*if(playerWinCount > computerWinCount){
         divScore.textContent = "Player wins!";
@@ -81,36 +89,19 @@ function game(choice, result){
 }
 
 
-
-function playerChoice(){
-    rockBtn.onclick = function(){
-        return choice;
-    }
-    paperBtn.onclick = function(){
-        return choice;
-    }
-    scissorsBtn.onclick = function(){
-        return choice;
-    }
-}
-
-let choice = playerChoice();
-
+// event listeners
 rockBtn.addEventListener("click", () => {
     let player = "rock";
     let comp = computerPlay();
-    let result = playRound(player, comp);
-    game(choice, result);
+    playRound(player, comp);
 });
 paperBtn.addEventListener("click", () => {
     let player = "paper";
     let comp = computerPlay();
-    let result = playRound(player, comp);
-    game(choice, result);
+    playRound(player, comp);
 });
 scissorsBtn.addEventListener("click", () => {
     let player = "scissors";
     let comp = computerPlay();
-    let result = playRound(player, comp);
-    game(choice, result);
+    playRound(player, comp);
 });
